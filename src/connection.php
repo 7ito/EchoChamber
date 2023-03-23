fe977c78dcace97bed09df63427496a1a078e4ec
Tito <justin@justintom.com>
2023-03-23 02:55:05 -0700

    updated sql connection details;

--- src/connection.php
+++ src/connection.php
@@ -1,5 +1,11 @@
 <?php
 
-    $conn = mysqli_connect("localhost", "root", "", "echochamber") or die("Unable to establish connection");
+    $host = "cosc360.ok.ubc.ca";
+    $username = "64216567";
+    $password = "64216567";
+    $dbname = "db_64216567";
+
+    $conn = mysqli_connect($host, $username, $password, $dbname);
+    // $conn = mysqli_connect("localhost", "root", "", "echochamber") or die("Unable to establish connection");
     
 ?>

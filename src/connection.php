11c25110240375c927a1b10f4f6bd1dce71a6a36
Tito <justin@justintom.com>
2023-04-06 01:34:53 -0700

    fixed connection details

--- src/connection.php
+++ src/connection.php
@@ -5,7 +5,7 @@
     $password = "64216567";
     $dbname = "db_64216567";
 
-    // $conn = mysqli_connect($host, $username, $password, $dbname) or die("Unable to establish connection");
-    $conn = mysqli_connect("localhost", "root", "", "echochamber") or die("Unable to establish connection");
+    $conn = mysqli_connect($host, $username, $password, $dbname) or die("Unable to establish connection");
+    // $conn = mysqli_connect("localhost", "root", "", "echochamber") or die("Unable to establish connection");
     
 ?>
